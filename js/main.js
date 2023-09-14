dfb06925bdee9408266359abfbddc6b0670a111d
VladNek <91415904+VladNek@users.noreply.github.com>
2023-09-14 16:31:40 +0300

    add:add  burger menu

--- js/main.js
+++ js/main.js
@@ -148,14 +148,13 @@ let isMobile = {
 if(isMobile.any()){
 		body.classList.add('touch');
 		let arrow=document.querySelectorAll('.arrow');
-	for(i=0; i<arrow.length; i++){
+ for(i=0; i<arrow.length; i++){
 			let thisLink=arrow[i].previousElementSibling;
-			let subMenu=arrow[i].nextElementSibling;
+			let subHeader=arrow[i].nextElementSibling;
 			let thisArrow=arrow[i];
-
 			thisLink.classList.add('parent');
-		arrow[i].addEventListener('click', function(){
-			subMenu.classList.toggle('open');
+	  arrow[i].addEventListener('click', function(){
+			subHeader.classList.toggle('open');
 			thisArrow.classList.toggle('active');
 		});
 	}
